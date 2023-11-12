1b616016f68d53d5f311da231cced2c3583c86be
axtamdev <axtam.dev@gmail.com>
2023-11-12 17:46:56 +0800

    update Map data handling

--- BusListing/src/screens/components/Map.js
+++ BusListing/src/screens/components/Map.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import MapView, { Marker }from 'react-native-maps'
 
-const Map = () => {
-    const markerCoordinates = { latitude: 22.37577, longitude: 114.10800 };
-22.37591011525515, 114.10833961302471
+const Map = ({long, lat}) => {
+    const markerCoordinates = {
+        latitude: lat, 
+        longitude: long
+    };
+
     return(
         <View style={styles.container}>
             <MapView style={styles.mapView}
         initialRegion={{
-          latitude: 22.37577,
-          longitude: 114.10800 ,
+          latitude: markerCoordinates.latitude,
+          longitude: markerCoordinates.longitude,
           latitudeDelta: 0.003,
           longitudeDelta: 0.003,
         }}>
