e7aa71201a971fbaaa6d64382b2b569930fd9957
axtamdev <axtam.dev@gmail.com>
2023-11-12 17:46:37 +0800

    Handle data filtering

---
+++ BusListing/src/utilities/busDataUtils.js
@@ -0,0 +1,43 @@
+export const handleData = (busData) => {
+  const currentTimestamp = new Date();
+  const dataArray = Array.isArray(busData.busStopETA) ? busData.busStopETA : [];
+
+  const filteredData = dataArray.reduce((acc, item) => {
+    const staTimestamp = new Date(item.eta);
+    const timeDifference = staTimestamp - currentTimestamp;
+    var minutesDifference
+    if (item.eta === null) {
+      minutesDifference = "!"
+    } else {
+      minutesDifference = Math.floor(timeDifference / (1000 * 60))
+      if (minutesDifference <= 0) {
+        minutesDifference = "-"
+      }
+    }
+
+
+    const existingItem = acc.find((existing) => existing.route === item.route);
+    if (!existingItem || minutesDifference < existingItem.minutesDifference) {
+      const newItem = {
+        route: item.route,
+        destination: item.dest_tc,
+        busStop: busData.busStopName,
+        minutesDifference: minutesDifference,
+        serviceType: item.service_type,
+        busStopId: busData.busStopId,
+        long: busData.busStopLong,
+        lat: busData.busStopLat
+      };
+      if (existingItem) {
+        acc.splice(acc.indexOf(existingItem), 1, newItem);
+      } else {
+        acc.push(newItem);
+      }
+    }
+
+    return acc;
+
+  }, []);
+
+  return filteredData;
+};
