f68a0c97c553fd64b25ab15664f502fd7e4a8c35
axtamdev <axtam.dev@gmail.com>
2023-11-12 17:48:35 +0800

    setup getETADate API handling

---
+++ BusListing/src/hook/getETADate.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+export const useGetETADate = (busStopId, route, service_type) => {
+    const [error, setError] = useState(null)
+    const [etaData, setETAData] = useState([])
+
+    useEffect(() => {
+        const fetchData= async () => {
+            try{
+                const data = await fetchETAData(busStopId, route, service_type)
+                setETAData(data)
+            }catch (error){
+                setError('Error')
+                throw error
+            }
+        }
+        fetchData()
+        
+        const timer = setInterval(fetchData, 5000)
+
+        return(()=>{
+            clearInterval(timer)
+        })
+    },[])
+
+    return [etaData]
+
+}
+
+export const fetchETAData = async ({busStopId, route, service_type}) => {
+    try {
+        const res = await fetch(`https://data.etabus.gov.hk/v1/transport/kmb/eta/${busStopId}/${route}/${service_type}`)
+
+        const data = await res.json()
+        return data
+    } catch (error) {
+        // setError('Error')
+        throw error
+    }
+}
