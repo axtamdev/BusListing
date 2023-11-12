2c4a364f5995fa4a394453354547c16d8aaf164c
axtamdev <axtam.dev@gmail.com>
2023-11-12 17:47:19 +0800

    update busDetail UI & data handling

---
+++ BusListing/src/screens/BusDetail.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from "react";
+import { StyleSheet, View, Text} from "react-native";
+import Map from "./components/Map";
+import ETAListView from "./components/ETAListView";
+// import {getETADate} from '../hook/getETAData';
+import { useGetETADate} from "../hook/getETADate";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        flex: 1
+    },
+    mapContainer: {
+        flex: 1
+    },
+    border: {
+        height: 4,
+        backgroundColor: 'red',
+    },
+    listContainer: {
+        flex: 1
+    }
+})
+
+export default function BusDetail({route, navigation}){
+    const {routeNumber, destination, busStopId, long, lat, busStopName, serviceType} = route.params
+    const [etaData] = useGetETADate({busStopId:busStopId, service_type: serviceType, route: routeNumber})
+
+    useEffect(()=>{
+        navigation.setOptions({title: routeNumber + " 往 " + destination})
+    })
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.mapContainer}>
+                <Map busStopId={busStopId} long={long} lat={lat}/>
+            </View>
+        <View style={styles.border}></View>
+            <View style={styles.listContainer}>
+                <ETAListView busData = {etaData} busStopName = {busStopName}/>
+            </View>
+        </View>
+    );
+}
